11a2e5734d475a6d9c2edb9fe1595254cd06edb3
dkrajzew <d.krajzewicz@gmail.com>
2026-03-21 17:57:35 +0100

    distinguishing date formats in tests

--- tests/util.py
+++ tests/util.py
@@ -38,9 +38,9 @@ def pdate(s):
     """Unify dates"""
     # https://www.google.com/search?client=firefox-b-d&q=pytthon+isoformat+regex
     regex1 = r'(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9]) (2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?'
-    s = re.sub(regex1, "<DATE>", s)
+    s = re.sub(regex1, "<DATE1>", s)
     regex2 = r'\w\w\w, \d\d \w\w\w \d\d\d\d \d\d:\d\d:\d\d [-+]\d\d\d\d'
-    return re.sub(regex2, "<DATE>", s)
+    return re.sub(regex2, "<DATE2>", s)
 
 
 def fread(filepath, patch_date=False):
@@ -55,10 +55,12 @@ def copy_template(tmp_path):
     """Copy the template to the test path"""
     shutil.copy(os.path.join((TEMPLATE_PATH), "data", "template.html"), str(tmp_path / "template.html"))
 
+
 def copy_files(tmp_path, files):
     """Copy named files to the test folder"""
     for file in files:
         shutil.copy(os.path.join((TEST_PATH), file), str(tmp_path / file))
+
 
 def copy_files_and_template(tmp_path, files):
     """Copy the template and named files to the test folder"""
