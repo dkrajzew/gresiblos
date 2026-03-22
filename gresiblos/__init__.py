b416866b23d1fb8d3b2a351e40c66d23fd5c2a00
dkrajzew <d.krajzewicz@gmail.com>
2026-03-22 14:20:56 +0100

    updating the __init__ file

--- gresiblos/__init__.py
+++ gresiblos/__init__.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python
-"""init module for gresiblos - greyrat's simple blog system."""
+"""Package initialization for gresiblos - greyrat's simple blog system."""
 # ===========================================================================
 # - https://github.com/dkrajzew/gresiblos
 # - http://www.krajzewicz.de
 # - contact me: daniel@krajzewicz.de
 # ===========================================================================
+
+
+# --- imports ---------------------------------------------------------------
+from .gresiblos import Template
+from .gresiblos import Entry
+from .gresiblos import PlainStorage
+from .gresiblos import load_template
+from .gresiblos import write_list
+from .gresiblos import write_feed
+from .gresiblos import get_args
+from .gresiblos import collect_files_sorted
+from .gresiblos import main
+from .gresiblos import script_run
+
+
+# --- definitions -----------------------------------------------------------
+__all__ = [
+    "Template",
+    "Entry",
+    "PlainStorage",
+    "load_template",
+    "write_list",
+    "write_feed",
+    "get_args",
+    "collect_files_sorted",
+    "main",
+    "script_run"
+]
+
